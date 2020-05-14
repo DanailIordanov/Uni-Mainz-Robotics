92c76e6f8778fbd7bd99d3eca5d1f81d3040301e
DanailIordanov <danail.iordanov@yahoo.com>
2020-05-14 17:23:26 +0200

    Imported openCV library and loaded image for recognition

--- src/Drive.py
+++ src/Drive.py
@@ -1,5 +1,17 @@
+import cv2
+
 def main():
     print("Hello World!")
+    src = cv2.imread('./../res/testPic.jpg', cv2.IMREAD_UNCHANGED)
+
+    img = cv2.resize(src, (src.shape[1] / 3, src.shape[0] / 3))
+    
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    
+    
+    
+    cv2.imshow("Image", img)
+    cv2.waitKey(0)
 
 
 if __name__=='__main__':
