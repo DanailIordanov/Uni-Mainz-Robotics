d8289c2bc8f148b7b0085c73350bc6173942a3b3
DanailIordanov <danail.iordanov@yahoo.com>
2020-06-20 22:28:55 +0200

    Continue driving until a KeyboardInterrupt

--- src/drive.py
+++ src/drive.py
@@ -1,8 +1,17 @@
 import robot.AlphaBot2
+import RPi.GPIO as GPIO
+import time
 
 if __name__ == "__main__":
-    # create all objects on which our code depends
+    # create an instance of our robot
     robot_obj = robot.AlphaBot2()
     
     # robot starts moving forward
     robot_obj.forward()
+    
+    # robot keeps moving forward until the program gets interrupted
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        GPIO.cleanup()
